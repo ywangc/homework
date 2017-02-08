1ce88d9c4bf763bd78b066e6050cf31511e84e12
ywang <yu.wang2013@hotmail.com>
2017-02-08 15:29:14 +0800

    New a project.

---
+++ ImageDisplayer/ImageDisplayerDoc.cpp
@@ -0,0 +1,147 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// ImageDisplayerDoc.cpp : implementation of the CImageDisplayerDoc class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "ImageDisplayer.h"
+#endif
+
+#include "ImageDisplayerDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CImageDisplayerDoc
+
+IMPLEMENT_DYNCREATE(CImageDisplayerDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CImageDisplayerDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CImageDisplayerDoc construction/destruction
+
+CImageDisplayerDoc::CImageDisplayerDoc()
+{
+	// TODO: add one-time construction code here
+
+}
+
+CImageDisplayerDoc::~CImageDisplayerDoc()
+{
+}
+
+BOOL CImageDisplayerDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: add reinitialization code here
+	// (SDI documents will reuse this document)
+
+	return TRUE;
+}
+
+
+
+
+// CImageDisplayerDoc serialization
+
+void CImageDisplayerDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: add storing code here
+	}
+	else
+	{
+		// TODO: add loading code here
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Support for thumbnails
+void CImageDisplayerDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Modify this code to draw the document's data
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Support for Search Handlers
+void CImageDisplayerDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Set search contents from document's data. 
+	// The content parts should be separated by ";"
+
+	// For example:  strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CImageDisplayerDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = NULL;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != NULL)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CImageDisplayerDoc diagnostics
+
+#ifdef _DEBUG
+void CImageDisplayerDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CImageDisplayerDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CImageDisplayerDoc commands
---
+++ ImageDisplayer/ImageDisplayerView.cpp
@@ -0,0 +1,137 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// ImageDisplayerView.cpp : implementation of the CImageDisplayerView class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "ImageDisplayer.h"
+#endif
+
+#include "ImageDisplayerDoc.h"
+#include "ImageDisplayerView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CImageDisplayerView
+
+IMPLEMENT_DYNCREATE(CImageDisplayerView, CView)
+
+BEGIN_MESSAGE_MAP(CImageDisplayerView, CView)
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CImageDisplayerView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CImageDisplayerView construction/destruction
+
+CImageDisplayerView::CImageDisplayerView()
+{
+	// TODO: add construction code here
+
+}
+
+CImageDisplayerView::~CImageDisplayerView()
+{
+}
+
+BOOL CImageDisplayerView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CImageDisplayerView drawing
+
+void CImageDisplayerView::OnDraw(CDC* /*pDC*/)
+{
+	CImageDisplayerDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here
+}
+
+
+// CImageDisplayerView printing
+
+
+void CImageDisplayerView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CImageDisplayerView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void CImageDisplayerView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add extra initialization before printing
+}
+
+void CImageDisplayerView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add cleanup after printing
+}
+
+void CImageDisplayerView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CImageDisplayerView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CImageDisplayerView diagnostics
+
+#ifdef _DEBUG
+void CImageDisplayerView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CImageDisplayerView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CImageDisplayerDoc* CImageDisplayerView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CImageDisplayerDoc)));
+	return (CImageDisplayerDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CImageDisplayerView message handlers
